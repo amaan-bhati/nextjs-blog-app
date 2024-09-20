ff9c1ea0b5d53fbe8ce49ccb2dc7e4dfe88a1ce5
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-09-20 21:18:47 +0530

    UI: text-highlight-component

---
+++ src/components/Highlight.js
@@ -0,0 +1,8 @@
+const Highlight = ({ children }) => (
+    <span style={{ backgroundColor: '#ffeb3b', fontWeight: 'bold' }}>
+      {children}
+    </span>
+  );
+  
+  export default Highlight;
+  
